6993c8a072c6bc8d5d72de4da19c9cfb3b68245c
Jakub Martin <jakub.wit.martin@gmail.com>
2023-02-06 01:46:45 +0100

    Continued.

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,134 @@
-//! An example of how to interact with wasm memory.
-//!
-//! Here a small wasm module is used to show how memory is initialized, how to
-//! read and write memory through the `Memory` object, and how wasm functions
-//! can trap when dealing with out-of-bounds addresses.
-
 // You can execute this example with `cargo run --example example`
 
+use std::collections::HashMap;
+use std::fmt::format;
+use std::fs::File;
+use std::io::Write;
+use std::iter::Map;
 use std::time::SystemTime;
 use anyhow::Result;
 use wasmtime::*;
 
+enum Node {
+    // Map(Box<Node>, Vec<Expr>, Vec<String>),
+    // Filter(Box<Node>, Expr),
+    Range(usize, usize),
+    Output(Box<Node>, String, usize), // source, field name, memory index
+}
+
+type ProduceFn<'a> = Box<dyn Fn(&mut ProduceContext, &HashMap<String, String>) + 'a>;
+
+impl Node {
+    fn generate(&self, ctx: &mut GenContext, produce: ProduceFn) {
+        match self {
+            Node::Range(start, end) => {
+                let i_name = ctx.get_unique("i");
+                ctx.buffer.push_str(&format!("(local ${} i32)\n", i_name));
+                ctx.buffer.push_str(&format!("(i32.const {})\n", start));
+                ctx.buffer.push_str(&format!("(local.set ${})\n", i_name));
+                let loop_name = ctx.get_unique("loop");
+                let mut fields = HashMap::new();
+                fields.insert("i".to_string(), i_name.to_string());
+                ctx.buffer.push_str(&format!("(loop ${}\n", &loop_name));
+                produce(&mut ProduceContext {
+                    continue_label: loop_name.clone(),
+                    gen_ctx: ctx,
+                }, &fields);
+                ctx.buffer.push_str(";; increment i\n");
+                ctx.buffer.push_str(&format!("(local.get ${})\n", i_name));
+                ctx.buffer.push_str(&format!("(i32.const 1)\n"));
+                ctx.buffer.push_str(&format!("(i32.add)\n"));
+                ctx.buffer.push_str(&format!("(local.set ${})\n", i_name));
+                ctx.buffer.push_str(";; check if loop finished\n");
+                ctx.buffer.push_str(&format!("(local.get ${})\n", i_name));
+                ctx.buffer.push_str(&format!("(i32.const {})\n", end));
+                ctx.buffer.push_str(&format!("(i32.lt_s)\n"));
+                ctx.buffer.push_str(&format!("(br_if ${})\n", &loop_name));
+                ctx.buffer.push_str(&format!(")\n"));
+            }
+            Node::Output(source, field, memory_index) => {
+                let output_ptr_name = ctx.get_unique("output_ptr");
+                ctx.buffer.push_str(&format!("(local ${} i32)\n", &output_ptr_name));
+                source.generate(ctx, Box::new(|ctx, fields| {
+                    let field_name = fields.get(field).unwrap();
+                    ctx.gen_ctx.buffer.push_str(";; store output\n");
+                    ctx.gen_ctx.buffer.push_str(&format!("(local.get ${})\n", &output_ptr_name));
+                    ctx.gen_ctx.buffer.push_str(&format!("(local.get ${})\n", &field_name));
+                    ctx.gen_ctx.buffer.push_str(&format!("(i32.store {})\n", memory_index));
+                    ctx.gen_ctx.buffer.push_str(";; update output pointer\n");
+                    ctx.gen_ctx.buffer.push_str(&format!("(local.get ${})\n", &output_ptr_name));
+                    ctx.gen_ctx.buffer.push_str(&format!("(i32.const {})\n", 4));
+                    ctx.gen_ctx.buffer.push_str(&format!("(i32.add)\n"));
+                    ctx.gen_ctx.buffer.push_str(&format!("(local.set ${})\n", &output_ptr_name));
+                }));
+            }
+        }
+    }
+}
+
+enum Expr {
+    Variable(String),
+    // Constant(Value),
+    Add(Box<Expr>, Box<Expr>),
+}
+
+struct GenContext {
+    unique_name_number: usize,
+    buffer: String,
+}
+
+impl GenContext {
+    fn get_unique(&mut self, name: &str) -> String {
+        let out = format!("{}_{}", name, self.unique_name_number);
+        self.unique_name_number += 1;
+        out
+    }
+}
+
+struct ProduceContext<'a> {
+    continue_label: String,
+    gen_ctx: &'a mut GenContext,
+}
+
 fn main() -> Result<()> {
+    let plan = Node::Range(3, 10);
+    let plan = Node::Output(Box::new(plan), "i".to_string(), 1);
+
+    let mut gen_ctx = GenContext {
+        unique_name_number: 0,
+        buffer: String::new(),
+    };
+    gen_ctx.buffer.push_str("(module\n");
+    gen_ctx.buffer.push_str(r#"(import "env" "input" (memory 0))
+(memory (export "memory") 0)
+(func (export "execute")
+"#);
+
+    plan.generate(&mut gen_ctx, Box::new(|ctx, fields| {}));
+
+    gen_ctx.buffer.push_str(")\n");
+    gen_ctx.buffer.push_str(")\n");
+
+    let mut file = File::create("out.wat")?;
+    file.write_all(gen_ctx.buffer.as_bytes())?;
+
     // Create our `store_fn` context and then compile a module and create an
     // instance from the compiled module all in one go.
     let mut config = Config::default();
     config.wasm_multi_memory(true);
     let mut store: Store<()> = Store::new(&Engine::new(&config).unwrap(), ());
     let start = SystemTime::now();
-    let module = Module::from_file(store.engine(), "src/memory.wat")?;
+    // let module = Module::from_binary(store.engine(), gen_ctx.buffer.as_bytes())?;
+    let module = Module::from_file(store.engine(), "out.wat")?;
     println!("Module compiled in {:?}", start.elapsed()?);
     let input_memory = Memory::new(&mut store, MemoryType::new(64, None)).unwrap();
-    input_memory.grow(&mut store, 4).unwrap();
-    let count = 4;
-    let start_pointer = 4*2;
-    let end_pointer = start_pointer + 4*count;
-    (&mut input_memory.data_mut(&mut store)[0..4]).copy_from_slice(&(start_pointer as u32).to_le_bytes());
-    (&mut input_memory.data_mut(&mut store)[4..8]).copy_from_slice(&(end_pointer as u32).to_le_bytes());
-    for i in (0 as usize)..(count as usize) {
-        (&mut input_memory.data_mut(&mut store)[start_pointer + i * 4..start_pointer + (i + 1) * 4]).copy_from_slice(&(i as u32).to_le_bytes());
-    }
+    // let start_pointer = 4 * 2;
+    // let end_pointer = start_pointer + 4 * count;
+    // (&mut input_memory.data_mut(&mut store)[0..4]).copy_from_slice(&(start_pointer as u32).to_le_bytes());
+    // (&mut input_memory.data_mut(&mut store)[4..8]).copy_from_slice(&(end_pointer as u32).to_le_bytes());
+    // for i in (0 as usize)..(count as usize) {
+    //     (&mut input_memory.data_mut(&mut store)[start_pointer + i * 4..start_pointer + (i + 1) * 4]).copy_from_slice(&(i as u32).to_le_bytes());
+    // }
 
     let instance = Instance::new(&mut store, &module, &[
         Extern::Memory(input_memory),
@@ -40,38 +140,18 @@ fn main() -> Result<()> {
         .get_memory(&mut store, "memory")
         .ok_or(anyhow::format_err!("failed to find `memory` export"))?;
     memory.grow(&mut store, 4)?;
-    // assert_eq!(memory.size(&store), 1024);
-    // assert_eq!(memory.data_size(&store), 65_536*1024);
-    // // memory.data_ptr()
-    // for i in (0 as usize)..(10_000_000 as usize) {
-    //     (&mut memory.data_mut(&mut store)[i * 4..(i + 1) * 4]).copy_from_slice(&((i%10) as u32).to_le_bytes());
-    // }
-    // let mut sum: u32 = 0;
-    // for i in 0..10_000_000 {
-    //     sum += u32::from_le_bytes(memory.data(&store)[i * 4..(i + 1) * 4].try_into().unwrap());
-    //     // sum += i;
-    // }
-
-    println!("{:?}", module.imports().collect::<Vec<_>>());
 
     let execute_fn = instance.get_typed_func::<(), ()>(&mut store, "execute")?;
-
 
     println!("Executing after {:?}", start.elapsed()?);
     execute_fn.call(&mut store, ())?;
     println!("Executed once after {:?}", start.elapsed()?);
-    // execute_fn.call(&mut store, ())?;
-    // println!("Executed twice after {:?}", start.elapsed()?);
-
-    // let memory = instance
-    //     .get_memory(&mut store, "memory")
-    //     .ok_or(anyhow::format_err!("failed to find `memory` export"))?;
 
     let output_memory = instance.exports(&mut store).next().unwrap().into_memory().unwrap();
 
     // assert_eq!(u32::from_le_bytes(memory.data(&store)[0..4].try_into().unwrap()), 4*10_000_000);
     // assert_eq!(sum, 45000000);
-    println!("Memory state: {:?}", input_memory.data(&store)[0..16].to_vec());
+    // println!("Memory state: {:?}", input_memory.data(&store)[0..16].to_vec());
     println!("Memory state: {:?}", output_memory.data(&store)[0..16].to_vec());
     assert_eq!(u32::from_le_bytes(output_memory.data(&store)[4..8].try_into().unwrap()), 6);
 
